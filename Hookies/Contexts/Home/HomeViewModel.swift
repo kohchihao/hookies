7f85b9582b934886bc4b503fdd54fd4f38f2c9ab
Koh Jun Wei <kjunwei1996@gmail.com>
2020-03-17 19:34:48 +0800

    Remove test code

--- Hookies/Contexts/Home/HomeViewModel.swift
+++ Hookies/Contexts/Home/HomeViewModel.swift
@@ -12,26 +12,4 @@ protocol HomeViewModelRepresentable {
 }
 
 class HomeViewModel: HomeViewModelRepresentable {
-    init() {
-        API.shared.gameplay.subscribeToPlayerStates(gameId: "JR2yO21QQAVabXNcAV3A") { states, error in
-            print(states)
-        }
-//        let players = ["EwIxUZMUE5STS2AJ1cDK9vZHcUc2", "3DlgEMdZCGSwaoHNGsVI9TvFMDE2"]
-//        var gameplay = Gameplay(gameId: "JR2yO21QQAVabXNcAV3A", playersId: players)
-//        let powerup = HookPowerup(isActivated: true, ownerId: "EwIxUZMUE5STS2AJ1cDK9vZHcUc2")
-//        let playerGameState = [
-//            PlayerGameState(playerId: "EwIxUZMUE5STS2AJ1cDK9vZHcUc2",
-//                                position: Vector(x: 144, y: 23),
-//                                velocity: Vector(x: 300, y: 200),
-//                                powerup: powerup)
-//        ]
-//        gameplay.updatePlayerStates(with: playerGameState)
-//        API.shared.gameplay.save(gameplay: gameplay, completion: { gameplay, error in
-//            print(gameplay)
-//        })
-//        let gameId = "JR2yO21QQAVabXNcAV3A"
-//        API.shared.gameplay.get(gameId: gameId, completion: { gameplay, error in
-//            print(gameplay)
-//        })
-    }
 }
