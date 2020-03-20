82f9d459fc671c8dd83786607ad82744480cd78d
Tan LongBin <tanlongbin@u.nus.edu>
2020-03-20 14:45:27 +0800

    Add map id and costume id to lobby

--- Hookies/Models/Lobby.swift
+++ Hookies/Models/Lobby.swift
@@ -12,17 +12,24 @@ struct Lobby {
     private(set) var lobbyId: String
     private(set) var hostId: String
     private(set) var playersId: [String]
+    private(set) var selectedMapType: MapType?
+    private(set) var costumesId: [String: CostumeType]
 
     init(hostId: String) {
         lobbyId = UUID().uuidString
         self.hostId = hostId
         self.playersId = [hostId]
+        self.costumesId = [hostId: .Pink]
     }
 
-    init(hostId: String, playersId: [String]) {
-        lobbyId = UUID().uuidString
-        self.hostId = hostId
-        self.playersId = playersId
+    init(hostId: String, playersId: [String], costumesId: [String: CostumeType]) {
+        self.init(hostId: hostId)
+        for playerId in playersId {
+            addPlayerId(playerId: playerId)
+        }
+        for (playerId, costumeType) in costumesId {
+            addCostumeId(playerId: playerId, costumeType: costumeType)
+        }
     }
 
     mutating func addPlayerId(playerId: String) {
@@ -30,5 +37,15 @@ struct Lobby {
             return
         }
         playersId.append(playerId)
+    }
+
+    mutating func addCostumeId(playerId: String, costumeType: CostumeType) {
+        if playersId.contains(playerId) {
+            self.costumesId[playerId] = costumeType
+        }
+    }
+
+    mutating func updateSelectedMapType(selectedMapType: MapType) {
+        self.selectedMapType = selectedMapType
     }
 }
