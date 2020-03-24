04ade21d053b154e3d3945559677362a6b03e23f
Marcus Koh <kohchihao@gmail.com>
2020-03-24 10:32:08 +0800

    Add basic Entity

---
+++ Hookies/Game/Entity/Bolt.swift
@@ -0,0 +1,21 @@
+//
+//  Bolt.swift
+//  Hookies
+//
+//  Created by Marcus Koh on 24/3/20.
+//  Copyright © 2020 Hookies. All rights reserved.
+//
+
+import Foundation
+
+class Bolt: Entity {
+    var components: [Component]
+
+    init(components: [Component]) {
+        self.components = components
+    }
+
+    convenience init() {
+        self.init(components: [])
+    }
+}
