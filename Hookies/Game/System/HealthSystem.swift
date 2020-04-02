d24888e09d57b04877190ab81f6b9c3d51243851
Marcus Koh <kohchihao@gmail.com>
2020-04-02 23:26:51 +0800

    Add health system

---
+++ Hookies/Game/System/HealthSystem.swift
@@ -0,0 +1,35 @@
+//
+//  HealthSystem.swift
+//  Hookies
+//
+//  Created by Marcus Koh on 2/4/20.
+//  Copyright © 2020 Hookies. All rights reserved.
+//
+
+import Foundation
+import CoreGraphics
+
+/// A player dies only if he goes too low. Below a fixed horizontal line.
+
+protocol HealthSystemProtocol {
+    func isPlayerAlive(for sprite: SpriteComponent) -> Bool
+    func respawnPlayer(for sprite: SpriteComponent) -> SpriteComponent
+}
+
+class HealthSystem: System, HealthSystemProtocol {
+
+    private let deathHorizontalLine = CGFloat(-1_000)
+    private let spawnHorizontalLine = CGFloat(500)
+
+    func isPlayerAlive(for sprite: SpriteComponent) -> Bool {
+        if sprite.node.position.y <= deathHorizontalLine {
+            return true
+        }
+        return false
+    }
+
+    func respawnPlayer(for sprite: SpriteComponent) -> SpriteComponent {
+        sprite.node.position = CGPoint(x: sprite.node.position.x, y: spawnHorizontalLine)
+        return sprite
+    }
+}
