1292f302197ad791132a87fb122c1624f428f2dd
Marcus Koh <kohchihao@gmail.com>
2020-04-02 13:20:14 +0800

    Add tests for sprite system

--- Hookies/Game/System/SpriteSystem.swift
+++ Hookies/Game/System/SpriteSystem.swift
@@ -11,31 +11,36 @@ import SpriteKit
 /// Only manages the creating and updating of sprite attributes
 
 protocol SpriteSystemProtocol {
-    func set(sprite: SpriteComponent,
-             of type: SpriteType,
-             with imageName: String,
-             at position: CGPoint
+    func set(
+        sprite: SpriteComponent,
+        of type: SpriteType,
+        with imageName: String,
+        at position: CGPoint
     ) -> SpriteComponent
-    func setPhysicsBody(to sprite: SpriteComponent,
-                        of type: SpriteType,
-                        rectangleOf size: CGSize
+    func setPhysicsBody(
+        to sprite: SpriteComponent,
+        of type: SpriteType,
+        rectangleOf size: CGSize
     ) -> SpriteComponent
-    func setPhysicsBody(to sprite: SpriteComponent,
-                        of type: SpriteType,
-                        circleOfRadius radius: CGFloat
+    func setPhysicsBody(
+        to sprite: SpriteComponent,
+        of type: SpriteType,
+        circleOfRadius radius: CGFloat
     ) -> SpriteComponent
-    func setPhysicsBody(to sprite: SpriteComponent,
-                        of type: SpriteType,
-                        circleOfRadius radius: CGFloat,
-                        center: CGPoint
+    func setPhysicsBody(
+        to sprite: SpriteComponent,
+        of type: SpriteType,
+        circleOfRadius radius: CGFloat,
+        center: CGPoint
     ) -> SpriteComponent
 }
 
 class SpriteSystem: System, SpriteSystemProtocol {
-    func set(sprite: SpriteComponent,
-             of type: SpriteType,
-             with imageName: String,
-             at position: CGPoint
+    func set(
+        sprite: SpriteComponent,
+        of type: SpriteType,
+        with imageName: String,
+        at position: CGPoint
     ) -> SpriteComponent {
         sprite.node = SKSpriteNode(imageNamed: imageName)
         sprite.node.position = position
@@ -47,29 +52,32 @@ class SpriteSystem: System, SpriteSystemProtocol {
 
     func set(sprite: SpriteComponent, to node: SKSpriteNode) -> SpriteComponent {
         sprite.node = node
-
         return sprite
     }
 
-    func setPhysicsBody(to sprite: SpriteComponent, of type: SpriteType, rectangleOf size: CGSize) -> SpriteComponent {
+    func setPhysicsBody(
+        to sprite: SpriteComponent,
+        of type: SpriteType,
+        rectangleOf size: CGSize
+    ) -> SpriteComponent {
         sprite.node.physicsBody = SKPhysicsBody(rectangleOf: size)
-
         return setPhysicsBodyProperties(to: sprite, of: type)
     }
 
-    func setPhysicsBody(to sprite: SpriteComponent,
-                        of type: SpriteType,
-                        circleOfRadius radius: CGFloat
+    func setPhysicsBody(
+        to sprite: SpriteComponent,
+        of type: SpriteType,
+        circleOfRadius radius: CGFloat
     ) -> SpriteComponent {
         sprite.node.physicsBody = SKPhysicsBody(circleOfRadius: radius)
-
         return setPhysicsBodyProperties(to: sprite, of: type)
     }
 
-    func setPhysicsBody(to sprite: SpriteComponent,
-                        of type: SpriteType,
-                        circleOfRadius radius: CGFloat,
-                        center: CGPoint
+    func setPhysicsBody(
+        to sprite: SpriteComponent,
+        of type: SpriteType,
+        circleOfRadius radius: CGFloat,
+        center: CGPoint
     ) -> SpriteComponent {
         sprite.node.physicsBody = SKPhysicsBody(circleOfRadius: radius, center: center)
 
