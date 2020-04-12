1d74a224a87de4ac38273fdd21cf0e1acaa7466a
Jin Ying <kenneth.jyt@gmail.com>
2020-04-12 21:30:09 +0800

    Add PlayerType and BotType

---
+++ Hookies/Shared/Enumerations/PlayerType.swift
@@ -0,0 +1,14 @@
+//
+//  PlayerType.swift
+//  Hookies
+//
+//  Created by JinYing on 12/4/20.
+//  Copyright © 2020 Hookies. All rights reserved.
+//
+
+import Foundation
+
+enum PlayerType {
+    case human
+    case bot
+}
