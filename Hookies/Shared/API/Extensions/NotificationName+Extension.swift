11cca5c3a9a3ca636ede51c9fec8fdd5b9f8e696
Jin Ying <kenneth.jyt@gmail.com>
2020-04-14 19:30:44 +0800

    NetworkManager: Add marks and comments

--- Hookies/Shared/API/Extensions/NotificationName+Extension.swift
+++ Hookies/Shared/API/Extensions/NotificationName+Extension.swift
@@ -9,24 +9,40 @@
 import Foundation
 
 extension Notification.Name {
-    // Systems to NetworkManager
-    static let receiveGameConnectionEvent = Notification.Name("receiveGameConnectionEvent")
-    static let receiveAddPlayersMapping = Notification.Name("receiveAddPlayersMapping")
+    // MARK: - Systems to NetworkManager
+    static let gameConnectionEvent = Notification.Name("gameConnectionEvent")
+    static let addPlayersMapping = Notification.Name("addPlayersMapping")
     static let broadcastGenericPlayerAction = Notification.Name("broadcastGenericPlayerAction")
     static let broadcastPowerupAction = Notification.Name("broadcastPowerupAction")
 
-    // NetworkManager to Systems
+    // MARK: - NetworkManager to Systems
+
+    // Start
     static let receivedOtherPlayerJoinEvent = Notification.Name("receivedOtherPlayerJoinEvent")
+
+    // UserConnection
     static let receivedOtherPlayerDisconnectedEvent = Notification.Name("receivedOtherPlayerDisconnectedEvent")
     static let receivedOtherPlayerRejoinEvent = Notification.Name("receivedOtherPlayerRejoinEvent")
+
+    // Hook
     static let receivedHookAction = Notification.Name("receivedHookAction")
     static let receivedUnookAction = Notification.Name("receivedUnookAction")
     static let receivedShortenRopeAction = Notification.Name("receivedShortenRopeAction")
     static let receivedLengthenRopeAction = Notification.Name("receivedLengthenRopeAction")
+
+    // Cannon
     static let receivedLaunchAction = Notification.Name("receivedLaunchAction")
+
+    // Deadlock
     static let receivedJumpAction = Notification.Name("receivedJumpAction")
+
+    // Health
     static let receivedRespawnAction = Notification.Name("receivedRespawnAction")
+
+    // FinishingLine
     static let receivedReachedFinishLineAction = Notification.Name("receivedReachedFinishLineAction")
+
+    // Powerup
     static let receviedPowerupCollectionEvent = Notification.Name("receivedPowerupCollectionEvent")
     static let receivedPowerupAction = Notification.Name("receivedPowerupAction")
 }
