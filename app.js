aab82124234b193c697c479142116968ab6d4851
Koh Jun Wei <kjunwei1996@gmail.com>
2020-04-19 21:27:49 +0800

    Revert

--- app.js
+++ app.js
@@ -1,8 +1,6 @@
 const app = require('express')();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http);
-io.set('heartbeat interval', 2);
-io.set('heartbeat timeout', 4);
 
 const GameManager = require('./GameManager');
 const LobbyManager = require('./LobbyManager');
