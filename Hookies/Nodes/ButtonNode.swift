e63df9cd1dfc4c0438be5970b572eb00f807987a
Marcus Koh <kohchihao@gmail.com>
2020-04-25 21:50:40 +0800

    Add docs for nodes

--- Hookies/Nodes/ButtonNode.swift
+++ Hookies/Nodes/ButtonNode.swift
@@ -8,50 +8,39 @@
 
 import SpriteKit
 
+/// Represents a button within the game.
+
 enum ButtonNodeState {
     case ButtonNodeStateActive, ButtonNodeStateSelected, ButtonNodeStateDisabled, ButtonNodeStateHidden
 }
 
 class ButtonNode: SKSpriteNode {
 
-    /* Setup a dummy action closure */
     var touchBeganHandler: () -> Void = {  }
     var touchEndHandler: () -> Void = {  }
 
     var initialButtonCenter: CGPoint?
 
-    /* Button state management */
     var state: ButtonNodeState = .ButtonNodeStateActive {
         didSet {
             switch state {
             case .ButtonNodeStateActive:
-                /* Enable touch */
                 self.isUserInteractionEnabled = true
-
-                /* Visible */
                 self.alpha = 1
             case .ButtonNodeStateSelected:
-                /* Semi transparent */
                 self.alpha = 0.7
             case .ButtonNodeStateDisabled:
                 self.isUserInteractionEnabled = false
                 self.alpha = 0.2
             case .ButtonNodeStateHidden:
-                /* Disable touch */
                 self.isUserInteractionEnabled = false
-                /* Hide */
                 self.alpha = 0
             }
         }
     }
 
-    /* Support for NSKeyedArchiver (loading objects from SK Scene Editor */
     required init?(coder aDecoder: NSCoder) {
-
-        /* Call parent initializer e.g. SKSpriteNode */
         super.init(coder: aDecoder)
-
-        /* Enable touch on button node */
         self.isUserInteractionEnabled = true
     }
 
