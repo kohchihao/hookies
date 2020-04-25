1d87394fcb437aa9bb83d5cabb56758522e5b27f
Koh Jun Wei <kjunwei1996@gmail.com>
2020-04-25 19:09:26 +0800

    Uncomment

--- HookiesTests/Game/System/DeadlockSystemTests.swift
+++ HookiesTests/Game/System/DeadlockSystemTests.swift
@@ -11,55 +11,55 @@ import XCTest
 import SpriteKit
 class DeadlockSystemTests: XCTestCase {
 
-//    var deadlockSystem: DeadlockSystem!
-//    var spriteSystem: SpriteSystem!
-//
-//    var playerSprite: SpriteComponent!
-//    var playerEntity: PlayerEntity!
-//
-//    override func setUp() {
-//        super.setUp()
-//        spriteSystem = SpriteSystem()
-//        playerEntity = PlayerEntity()
-//        playerSprite = SpriteComponent(parent: playerEntity)
-//        _ = spriteSystem.set(sprite: playerSprite, of: .player1, with: "Pink_Monster", at: CGPoint(x: 10, y: 20))
-//        _ = spriteSystem.setPhysicsBody(to: playerSprite, of: .player1)
-//        deadlockSystem = DeadlockSystem(sprite: playerSprite)
-//    }
-//
-//    override func tearDown() {
-//        deadlockSystem = nil
-//        spriteSystem = nil
-//
-//        playerSprite = nil
-//        playerEntity = nil
-//        super.tearDown()
-//    }
-//
-//    func testCheckIfStuck_playerStuck_velocityNearZero() {
-//        playerSprite.node.physicsBody?.velocity = CGVector(dx: 0.2, dy: 100)
-//        let isStuck = deadlockSystem.checkIfStuck()
-//        XCTAssertTrue(isStuck)
-//    }
-//
-//    func testCheckIfStuck_playerNotStuck() {
-//        playerSprite.node.physicsBody?.velocity = CGVector(dx: 0.7, dy: 100)
-//        let isStuck = deadlockSystem.checkIfStuck()
-//        XCTAssertFalse(isStuck)
-//    }
-//
-//    func testResolveDeadlock() {
-//        playerSprite.node.physicsBody?.velocity = CGVector(dx: 0.2, dy: 100)
-//        deadlockSystem.resolveDeadlock()
-//        XCTAssertNotEqual(CGVector(dx: 0.2, dy: 100), playerSprite.node.physicsBody?.velocity)
-//    }
-//
-//    func testResolveDeadlockWithPosition() {
-//        playerSprite.node.physicsBody?.velocity = CGVector(dx: 0.2, dy: 100)
-//        deadlockSystem.resolveDeadlock(
-//            for: playerSprite,
-//            at: playerSprite.node.position,
-//            with: playerSprite.node.physicsBody!.velocity)
-//        XCTAssertNotEqual(CGVector(dx: 0.2, dy: 100), playerSprite.node.physicsBody?.velocity)
-//    }
+    var deadlockSystem: DeadlockSystem!
+    var spriteSystem: SpriteSystem!
+
+    var playerSprite: SpriteComponent!
+    var playerEntity: PlayerEntity!
+
+    override func setUp() {
+        super.setUp()
+        spriteSystem = SpriteSystem()
+        playerEntity = PlayerEntity()
+        playerSprite = SpriteComponent(parent: playerEntity)
+        _ = spriteSystem.set(sprite: playerSprite, of: .player1, with: "Pink_Monster", at: CGPoint(x: 10, y: 20))
+        _ = spriteSystem.setPhysicsBody(to: playerSprite, of: .player1)
+        deadlockSystem = DeadlockSystem(sprite: playerSprite)
+    }
+
+    override func tearDown() {
+        deadlockSystem = nil
+        spriteSystem = nil
+
+        playerSprite = nil
+        playerEntity = nil
+        super.tearDown()
+    }
+
+    func testCheckIfStuck_playerStuck_velocityNearZero() {
+        playerSprite.node.physicsBody?.velocity = CGVector(dx: 0.2, dy: 100)
+        let isStuck = deadlockSystem.checkIfStuck()
+        XCTAssertTrue(isStuck)
+    }
+
+    func testCheckIfStuck_playerNotStuck() {
+        playerSprite.node.physicsBody?.velocity = CGVector(dx: 0.7, dy: 100)
+        let isStuck = deadlockSystem.checkIfStuck()
+        XCTAssertFalse(isStuck)
+    }
+
+    func testResolveDeadlock() {
+        playerSprite.node.physicsBody?.velocity = CGVector(dx: 0.2, dy: 100)
+        deadlockSystem.resolveDeadlock()
+        XCTAssertNotEqual(CGVector(dx: 0.2, dy: 100), playerSprite.node.physicsBody?.velocity)
+    }
+
+    func testResolveDeadlockWithPosition() {
+        playerSprite.node.physicsBody?.velocity = CGVector(dx: 0.2, dy: 100)
+        deadlockSystem.resolveDeadlock(
+            for: playerSprite,
+            at: playerSprite.node.position,
+            with: playerSprite.node.physicsBody!.velocity)
+        XCTAssertNotEqual(CGVector(dx: 0.2, dy: 100), playerSprite.node.physicsBody?.velocity)
+    }
 }
